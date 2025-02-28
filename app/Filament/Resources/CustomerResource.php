cbe41879dd1e2ca08a699abc8720d731ee29201c
belajar-vue-2 <aryaadi229@gmail.com>
2025-02-28 22:35:23 +0700

    first commit

---
+++ app/Filament/Resources/CustomerResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Customer;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\TextInput;
+use App\Filament\Resources\CustomerResource\Pages;
+use Filament\Tables\Columns\TextColumn;
+
+class CustomerResource extends Resource
+{
+    protected static ?string $model = Customer::class;
+    protected static ?string $navigationIcon = 'heroicon-o-user';
+    // protected static ?string $navigationLabel = 'Pelanggan';
+    protected static ?string $navigationGroup = 'Users';
+    // protected static ?string $slug = 'pelanggan';
+    // protected static ?string $modelLabel = 'Pelanggan';
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('Fullname')
+                    ->required()
+                    ->placeholder('Your fullname...'),
+                TextInput::make('address')
+                    ->required(),
+                TextInput::make('phone')
+                    ->numeric()
+                    ->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Fullname')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('address')
+                    ->copyable()
+                    ->copyMessage('Address is copied'),
+                TextColumn::make('phone'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCustomers::route('/'),
+            'create' => Pages\CreateCustomer::route('/create'),
+            'edit' => Pages\EditCustomer::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Product;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use App\Filament\Resources\ProductResource\Pages;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name'),
+                TextInput::make('qty'),
+                TextInput::make('price')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('qty'),
+                TextColumn::make('price')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
